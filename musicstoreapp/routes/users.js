a284fe28f064fdbffac11d91ee0b97328dfce404
EloyRsua <eloy.rubio.suarez@gmail.com>
2026-03-30 09:33:05 +0200

    SDI-2526-611-10.3-Mensajes y alertas.

--- musicstoreapp/routes/users.js
+++ musicstoreapp/routes/users.js
@@ -14,10 +14,9 @@ module.exports = function (app, usersRepository) {
     }
 //res.send('usuario registrado');
     usersRepository.insertUser(user).then(userId => {
-      res.redirect("/users/login");
+      res.redirect("/users/login" + "?message=Nuevo usuario registrado." + "&messageType=alert-info");
     }).catch(error => {
-      res.send("Error al insertar el usuario");
-    });
+      res.redirect("/users/signup" + "?message=Se ha producido un error al registrar el usuario." + "&messageType=alert-danger");    });
   });
   app.get('/users/login', function (req, res) {
     res.render("login.twig");
@@ -33,7 +32,11 @@ module.exports = function (app, usersRepository) {
     usersRepository.findUser(filter, options).then(user => {
       if (user == null) {
         req.session.user = null;
-        res.send("Usuario no identificado");
+        //res.send("Usuario no identificado");
+        res.redirect("/users/login" +
+            "?message=Email o password incorrecto"+
+            "&messageType=alert-danger ");
+
       } else {
         req.session.user = user.email;
         res.redirect("/publications");
