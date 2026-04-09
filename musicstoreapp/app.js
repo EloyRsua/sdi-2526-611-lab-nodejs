a8991e2431d1da17bd7506db6ac6fcf3811c43f4
EloyRsua <eloy.rubio.suarez@gmail.com>
2026-04-09 17:45:57 +0200

    SDI-2526-611-11.3-SW Cliente JQuery + AJAX.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -11,6 +11,15 @@ const { MongoClient } = require("mongodb");
 
 // 2. INICIALIZACIÓN DE LA APP
 const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, token");
+  // Debemos especificar todas las headers que se aceptan. Content-Type , token
+  next();
+});
+
 let jwt = require('jsonwebtoken');
 app.set('jwt', jwt);
 
