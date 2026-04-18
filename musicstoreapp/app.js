5f22bc824c7cb63f261d859f004fe87e9b0ac33f
EloyRsua <eloy.rubio.suarez@gmail.com>
2026-04-18 18:19:20 +0200

    SDI-2526-611-12.4-REACT Conexión con la API: Inicio de Sesión.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -62,11 +62,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Better than "*" for credentials
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+  res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT, OPTIONS"); // Added OPTIONS
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
-  next();
+
+  // Respond immediately to preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
 // Sesiones
